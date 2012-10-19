7d9a46112afcff376eb87332509420ea7d0cde15
lilin <lilin@jiuan.com>
2012-10-19 08:46:12 +0800

    change to an better performance

--- TCPForward_Gevent_phuslu.py
+++ TCPForward_Gevent_phuslu.py
@@ -5,39 +5,45 @@ import sys
 import os
 
 import gevent
-import gevent.monkey
 import gevent.server
 import gevent.queue
 
-import re
-import time
-import collections
+CONNECTION_QUEUE = gevent.queue.Queue()
+BUFFER_SIZE = 65536
 
-QUEUE_MAP = {}
+def io_copy(source, dest):
+    wfile = dest.makefile('wb', BUFFER_SIZE)
+    while 1:
+        wfile.write(source.recv(BUFFER_SIZE))
+
+def forward():
+    connection_map = {}
+    while 1:
+        myid, conn_type, conn = CONNECTION_QUEUE.get()
+        if myid not in connection_map:
+            connection_map[myid] = (conn_type, conn)
+        else:
+            _, conn_in_map = connection_map[myid]
+            if conn_type:
+                gevent.spawn(io_copy, conn, conn_in_map)
+            else:
+                gevent.spawn(io_copy, conn_in_map, conn)
 
 def producer_handler(sock, address):
     myid = sock.recv(1024)
-    while 1:
-        data = sock.recv(8192)
-        try:
-            QUEUE_MAP[myid].put(data)
-        except KeyError:
-            pass
+    CONNECTION_QUEUE.put((myid, 1, sock))
 
 def consumer_handler(sock, address):
     myid = sock.recv(1024)
-    QUEUE_MAP[myid] = queue = gevent.queue.Queue()
-    while 1:
-        data = queue.get()
-        sock.send(data)
+    CONNECTION_QUEUE.put((myid, 0, sock))
 
 def main():
     producer_server = gevent.server.StreamServer(('', 44445), producer_handler)
     producer_server.start()
     consumer_server = gevent.server.StreamServer(('', 44444), consumer_handler)
     consumer_server.start()
-    while True:
-    	gevent.sleep(100)
+    forward()
 
 if __name__ == '__main__':
     main()
+
